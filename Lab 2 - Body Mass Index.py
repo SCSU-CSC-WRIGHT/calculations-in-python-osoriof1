5af1953c1c54ba0abf19b2c248fc99e657007a51
osoriof1 <osoriof1@southernct.edu>
2024-09-23 20:42:40 +0000

    Converted temp

---
+++ Lab 2 - Body Mass Index.py
@@ -0,0 +1,4 @@
+w = int(input("Enter your weight(kg): "))
+h = int(input("Enter your height (m): "))
+body_mass_index = ((w/h))
+print(f"Your BMI is: {body_mass_index} ")
---
+++ Lab 3 - Temperature Change.py
@@ -0,0 +1,3 @@
+c = int(input("Enter the temperature in Celcius: "))
+conversion_fahrenheit = ((c * 9/5 + 32))
+print(f"The temperature in Fahrenheit is: {conversion_fahrenheit}")
